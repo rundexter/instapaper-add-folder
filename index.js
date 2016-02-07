aaad0be43c44acacbf458bc92502766063cefe72
Daniel Ilkovich <daniel+github@betaworks.com>
2016-02-06 20:11:12 -0500

    initial

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var Instapaper = require('instapaper')
+  , _          = require('lodash')
+  , q          = require('q')
+  , apiUrl     = 'https://www.instapaper.com/api/1.1'
+;
+
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +12,36 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var auth        = dexter.provider('instapaper').credentials() 
+          , client      = Instapaper(auth.consumer_key, auth.consumer_secret, {apiUrl: apiUrl})
+          , titles      = step.input('title')
+          , self        = this
+          , connections = []
+          , folders     = []
+        ;
+
+        if(!titles.length) return this.fail('A title is required');
+
+        client.setOAuthCredentials(auth.access_token, auth.access_token_secret);
+
+        _.each(titles,function(title) {
+            var deferred = q.defer();
+            client.bookmarks.client.request('/folders/add', {title: title})
+               .then(function(result) {
+                   folders.push({
+                       title : result.title
+                       , id  : result.folder_id
+                   });
+                   deferred.resolve();
+               }).catch(function(err) {
+                   deferred.reject(err);
+               });
+
+            connections.push(deferred.promise);
+        });
+
+        q.all(connections)
+           .then(this.complete.bind(this, folders))
+           .fail(this.fail.bind(this));
     }
 };
